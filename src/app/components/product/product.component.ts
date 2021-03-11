80dce97e7eb9d1c5ec3d887fd9300c3290153813
kaaneneskpc <kaaneneskpc1@gmail.com>
2021-03-11 14:48:21 +0300

    AngularJsWorks

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product',
@@ -7,14 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  product1: any = { productId: 1, productName: 'Glass', categoryId: 1, unitPrice: 5 };
-  product2: any = { productId: 1, productName: 'Keyboard', categoryId: 1, unitPrice: 5 };
-  product3: any = { productId: 1, productName: 'Mouse', categoryId: 1, unitPrice: 5 };
-  product4: any = { productId: 1, productName: 'Laptop', categoryId: 1, unitPrice: 5 };
-  product5: any = { productId: 1, productName: 'Camera', categoryId: 1, unitPrice: 5 };
+  
 
 
-  products = [this.product1, this.product2, this.product3, this.product4, this.product5];
+  products:Product[] = [
+    
+    
+
+  ];
 
 
   constructor() { }
