8cfc2b32b017a7c0ee2042c79afc337ee0f94e69
kaaneneskpc <kaaneneskpc1@gmail.com>
2021-03-21 14:11:31 +0300

    AngularAppProduct

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup,FormBuilder,FormControl,Validators} from '@angular/forms'
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 import { ToastrService } from 'ngx-toastr';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -12,43 +12,49 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductAddComponent implements OnInit {
 
 
-  productAddForm:FormGroup;
+  productAddForm: FormGroup;
 
-  constructor(private formBuilder:FormBuilder,private productService:ProductService,private toastrService:ToastrService) { }
+  constructor(private formBuilder: FormBuilder, private productService: ProductService, private toastrService: ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
   }
 
-  createProductAddForm(){
+  createProductAddForm() {
     this.productAddForm = this.formBuilder.group({
-      productName:["",Validators.required],
-      unitPrice:["",Validators.required],
-      unitsInStock:["",Validators.required],
-      categoryId:["",Validators.required]
-      
+      productName: ["", Validators.required],
+      unitPrice: ["", Validators.required],
+      unitsInStock: ["", Validators.required],
+      categoryId: ["", Validators.required]
+
     })
   }
 
-  add(){
-    
-    
-    if(this.productAddForm.valid){
-      let productModel = Object.assign({},this.productAddForm.value)
-      this.productService.add(productModel).subscribe(response=>{
-        console.log(response)
-        this.toastrService.success(response.message,"Success")
-      },responseError=>{
-        console.log(responseError)
-        this.toastrService.error(responseError)
+  add() {
+
+
+    if (this.productAddForm.valid) {
+      let productModel = Object.assign({}, this.productAddForm.value)
+      this.productService.add(productModel).subscribe(response => {
+
+        this.toastrService.success(response.message, "Success")
+      }, responseError => {
+        if (responseError.error.Errors.length > 0) {
+          for (let i = 0; i < responseError.error.Errors.length; i++) {
+            this.toastrService.error(responseError.error.Errors[i].ErrorMessage, "Validation Error")
+
+          }
+
+        }
+
       })
-      
-     }
-     else{
-       this.toastrService.error("Form is missing","Error")
-     }
-    
-    
+
+    }
+    else {
+      this.toastrService.error("Form is missing", "Error")
+    }
+
+
   }
 
 }
