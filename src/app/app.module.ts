c827b1d37de27a73b18ce00ed06b5dcb96a43d6b
kaaneneskpc <kaaneneskpc1@gmail.com>
2021-03-24 21:52:47 +0300

    ProductAppManagement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 import {ToastrModule} from "ngx-toastr";
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 
 
@@ -27,7 +29,9 @@ import { ProductAddComponent } from './components/product-add/product-add.compon
     VatAddedPipe,
     FilterPipePipe,
     CartSummaryComponent,
-    ProductAddComponent
+    ProductAddComponent,
+    LoginComponent
+
   ],
   imports: [
     BrowserModule,
@@ -39,7 +43,7 @@ import { ProductAddComponent } from './components/product-add/product-add.compon
     ToastrModule.forRoot({
       positionClass:"toast-bottom-right"
     }),
-    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
